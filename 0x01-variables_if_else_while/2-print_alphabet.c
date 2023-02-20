065adccf80abfec6b5f7ac82036e7369638e82f7
samuelemerite <samuelminkada5@gmail.com>
2023-02-20 15:09:24 -0800

    i am working

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 
-/**main - print alphabet in lowercase
+/**
+*main - print alphabet in lowercase
 *followed by new line
 *return: Always 0
 */
 int main(void)
 {
-    int i=0;
-    char alpha[]="abcdefghijklmnopqrstuvwxyz";
-    for(i=0;i<26; i++)
-    {
-        putchar(alpha[i]);
-    }
-    putchar('\n');
-    return 0;
+  int i=0;
+  char alpha[]="abcdefghijklmnopqrstuvwxyz";
+  for(i=0;i<26; i++)
+  {
+    putchar(alpha[i]);
+  }
+  putchar('\n');
+  return 0;
 }
