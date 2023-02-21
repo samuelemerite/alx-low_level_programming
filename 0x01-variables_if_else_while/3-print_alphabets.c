038013735543b6ccb668530fa1afe093f1290baa
samuelemerite <samuelminkada5@gmail.com>
2023-02-21 06:37:27 -0800

    i am working

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,18 @@
 
 
 /**
- *main - print alphabet in lowercase
- *followed by new line
- *return: Always 0
+ * main - print alphabet in lowercase
+ * followed by new line
+ * return: Always 0
  */
 int main(void)
 {
     char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
     int i = 0;
-    for (i=0; i<52; i++)
+    for ( i = 0; i < 52; i++)
     {
         putchar(alpha[i]);
     }
     putchar('\n');
-    return 0;
+    return (0);
 }
