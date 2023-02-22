dc5104357006273e09626641dbfc76704ac60a89
samuelemerite <samuelminkada5@gmail.com>
2023-02-22 02:18:11 -0800

    ok

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,23 +10,23 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if(j == 0 )
+if (j == 0)
 {
 _putchar('0');
 }
-if(i * j < 10 && j != 0)
+if (i * j < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar( (i * j) + '0');
 }
-else if( i * j >= 10)
+else if ( i * j >= 10)
 {
 _putchar(',');
 _putchar(' ');
-_putchar((i * j/10) + '0');
-_putchar(((i * j)%10) +'0');
+_putchar((i * j / 10) + '0');
+_putchar(((i * j) % 10) + '0');
 }
 }
 _putchar('\n');
