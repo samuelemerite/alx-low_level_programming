e3b824d7cb46212aad39b1e55e6a222b81f06cf6
samuelemerite <samuelminkada5@gmail.com>
2023-02-28 05:20:47 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - print the number of character of a word
+ * @s: string to print the value of length
+ */
+int _strlen(char *s)
+{
+int i = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+}
+return (i);
+}
